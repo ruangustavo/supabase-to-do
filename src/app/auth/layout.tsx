53960c2acf527411ea5124282443d72430343a13
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2024-04-07 17:51:07 -0300

    styles: update layout component to make the title hidden on small screens

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex-1 grid grid-cols-2">
-      <div className="p-8 bg-muted">
+    <div className="flex-1 grid sm:grid-cols-1 md:grid-cols-2">
+      <div className="p-8 bg-muted hidden md:block">
         <h1 className="text-xl font-semibold tracking-tight">To-do app</h1>
       </div>
       <div className="p-8 flex items-center justify-center">{children}</div>
